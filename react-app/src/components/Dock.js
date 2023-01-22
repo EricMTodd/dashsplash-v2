79cab627d79a91df8847685d58277ff20e8fa6cc
Eric Todd <39493826+EricMTodd@users.noreply.github.com>
2023-01-22 08:24:56 -0700

    Working on dock arrow

--- react-app/src/components/Dock.js
+++ react-app/src/components/Dock.js
@@ -1,12 +1,15 @@
 import Clock from './Clock'
 
 const Dock = () => {
+
   return(
     <div id='dock-container'>
       <div id='dock-haze'>
-
       </div>
       <div id='dock-content'>
+        <div id='dock-arrow-container'>
+          <i className='dock-arrow'></i>
+        </div>
         <Clock />
       </div>
     </div>
